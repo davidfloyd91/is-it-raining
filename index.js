6fac21267f5e874a4a9c8c6e0f41eb8b92170448
davidfloyd91 <davidfloyd91@gmail.com>
2019-05-23 16:56:50 -0400

    added dark sky api query

--- index.js
+++ index.js
@@ -12,33 +12,64 @@ const port = 3000;
 const googleKey = process.env.GOOGLE_API_KEY;
 const darkSkyKey = process.env.DARK_SKY_API_KEY;
 
-let lat, lng;
-
-const options = {
+const googleOptions = {
   hostname: 'www.googleapis.com',
   path: `/geolocation/v1/geolocate?key=${googleKey}`,
   method: 'POST',
   headers: { 'Content-Type': 'application/json' }
 };
 
+let lat, lng, darkSkyOptions;
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
 app.get('/location', (req, res) => {
-  const request = https.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
+  const request = https.request(googleOptions, (response) => {
+    console.log(`STATUS: ${response.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+    response.setEncoding('utf8');
+    response.on('data', (chunk) => {
       console.log(`BODY: ${chunk}`);
 
       let coords = JSON.parse(chunk);
-      lat = coords.location.lat;
-      lng = coords.location.lng;
+      lat = coords.location.lat.toFixed(3);
+      lng = coords.location.lng.toFixed(3);
+
+      setDarkSkyOptions();
     });
 
-    res.on('end', () => {
+    response.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
+
+  request.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  request.end();
+});
+
+const setDarkSkyOptions = () => {
+  darkSkyOptions = {
+    hostname: 'api.darksky.net',
+    path: `/forecast/${darkSkyKey}/${lat},${lng}`,
+    headers: { 'Content-Type': 'application/json' }
+  };
+};
+
+app.get('/weather', (req, res) => {
+  const request = https.request(darkSkyOptions, (response) => {
+    console.log(`STATUS: ${response.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+    response.setEncoding('utf8');
+    response.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+
+    response.on('end', () => {
       console.log('No more data in response.');
     });
   });
