ba9b138d7390fb7fcf80d6c3e552de73794569c8
davidfloyd91 <davidfloyd91@gmail.com>
2019-05-24 00:35:55 -0400

    so now we're getting amazon servers' location

--- index.js
+++ index.js
@@ -10,50 +10,50 @@ const cors = require('cors');
 const app = express();
 const port = 8081;
 
-const googleKey = process.env.GOOGLE_API_KEY;
+// const googleKey = process.env.GOOGLE_API_KEY;
 const darkSkyKey = process.env.DARK_SKY_API_KEY;
 
 const corsOptions = {
   origin: '*'
 }
 
-const googleOptions = {
-  hostname: 'www.googleapis.com',
-  path: `/geolocation/v1/geolocate?key=${googleKey}`,
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' }
-};
+// const googleOptions = {
+//   hostname: 'www.googleapis.com',
+//   path: `/geolocation/v1/geolocate?key=${googleKey}`,
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json' }
+// };
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.get('/location', cors(corsOptions), (req, res) => {
-  let data = '';
-
-  const request = https.request(googleOptions, (response) => {
-    console.log(`STATUS: ${response.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-    response.setEncoding('utf8');
-    response.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-      data += chunk;
-    });
-
-    response.on('end', () => {
-      console.log('No more data in response.');
-
-      let coords = JSON.parse(data);
-      res.send(coords);
-    });
-  });
-
-  request.on('error', (e) => {
-    console.error(`Problem with request: ${e.message}`);
-  });
-
-  request.end();
-});
+// app.get('/location', cors(corsOptions), (req, res) => {
+//   let data = '';
+//
+//   const request = https.request(googleOptions, (response) => {
+//     console.log(`STATUS: ${response.statusCode}`);
+//     console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+//     response.setEncoding('utf8');
+//     response.on('data', (chunk) => {
+//       console.log(`BODY: ${chunk}`);
+//       data += chunk;
+//     });
+//
+//     response.on('end', () => {
+//       console.log('No more data in response.');
+//
+//       let coords = JSON.parse(data);
+//       res.send(coords);
+//     });
+//   });
+//
+//   request.on('error', (e) => {
+//     console.error(`Problem with request: ${e.message}`);
+//   });
+//
+//   request.end();
+// });
 
 app.get('/weather/:lat,:lng', cors(corsOptions), (req, res) => {
   const lat = req.params.lat;
