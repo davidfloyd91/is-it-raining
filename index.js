5111919f656f68fc7bca62ce1db692b6505641ce
davidfloyd91 <davidfloyd91@gmail.com>
2019-05-23 23:00:21 -0400

    switched lat and lng to params -  /weather/:lat,:lng

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // $ curl -X GET -H 'Content-Type: application/json' http://localhost:8081/location
 // $ curl -X GET -H 'Content-Type: application/json' http://localhost:8081/weather
 
+// $ curl -X GET -H 'Content-Type: application/json' http://is-it-raining-env.mqxjxhgsyd.us-east-1.elasticbeanstalk.com/location
+// $ curl -X GET -H 'Content-Type: application/json' http://is-it-raining-env.mqxjxhgsyd.us-east-1.elasticbeanstalk.com/weather
+
+
 const express = require('express');
 const https = require('https');
 const app = express();
@@ -9,7 +13,7 @@ const port = 8081;
 const googleKey = process.env.GOOGLE_API_KEY;
 const darkSkyKey = process.env.DARK_SKY_API_KEY;
 
-let lat, lng, darkSkyOptions, minutelyForecast, isItRaining;
+// let lat, lng, darkSkyOptions, minutelyForecast, isItRaining;
 
 const googleOptions = {
   hostname: 'www.googleapis.com',
@@ -37,9 +41,9 @@ app.get('/location', (req, res) => {
     response.on('end', () => {
       let coords = JSON.parse(data);
 
-      lat = coords.location.lat.toFixed(3);
-      lng = coords.location.lng.toFixed(3);
-      setDarkSkyOptions();
+      // lat = coords.location.lat.toFixed(3);
+      // lng = coords.location.lng.toFixed(3);
+      // setDarkSkyOptions();
 
       res.send(coords);
       console.log('No more data in response.');
@@ -53,18 +57,27 @@ app.get('/location', (req, res) => {
   request.end();
 });
 
-const setDarkSkyOptions = () => {
-  darkSkyOptions = {
+// const setDarkSkyOptions = () => {
+  // darkSkyOptions = {
+  //   hostname: 'api.darksky.net',
+  //   path: `/forecast/${darkSkyKey}/${lat},${lng}`,
+  //   headers: { 'Content-Type': 'application/json' }
+  // };
+// };
+
+app.get('/weather/:lat,:lng', (req, res) => {
+  const lat = req.params.lat;
+  const lng = req.params.lng;
+
+  let data = '';
+
+  const darkSkyOptions = {
     hostname: 'api.darksky.net',
     path: `/forecast/${darkSkyKey}/${lat},${lng}`,
     headers: { 'Content-Type': 'application/json' }
   };
-};
 
-app.get('/weather', (req, res) => {
-  let data = '';
-
-  if (darkSkyOptions) {
+  // if (darkSkyOptions) {
     const request = https.request(darkSkyOptions, (response) => {
       console.log(`STATUS: ${response.statusCode}`);
       console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
@@ -78,11 +91,11 @@ app.get('/weather', (req, res) => {
         console.log('No more data in response.');
 
         let parsed = JSON.parse(data);
-        minutelyForecast = parsed.minutely;
-
-        if (minutelyForecast) {
-          setIsItRaining();
-        };
+        // minutelyForecast = parsed.minutely;
+        //
+        // if (minutelyForecast) {
+        //   setIsItRaining();
+        // };
 
         res.send(parsed);
       });
@@ -93,26 +106,26 @@ app.get('/weather', (req, res) => {
     });
 
     request.end();
-  } else {
-    console.log('Cannot complete query, coordinates unavailable.');
-  };
+  // } else {
+  //   console.log('Cannot complete query, coordinates unavailable.');
+  // };
 });
 
-const setIsItRaining = () => {
-  let precipArr = minutelyForecast.data.map(min => {
-    return min.precipProbability;
-  });
-
-  precipArr.forEach(prob => {
-    if (prob >= 0.5) {
-      isItRaining = 2;
-    } else if (prob >= 0.1) {
-      isItRaining = 1;
-    } else {
-      isItRaining = 0;
-    };
-  });
-};
+// const setIsItRaining = () => {
+//   let precipArr = minutelyForecast.data.map(min => {
+//     return min.precipProbability;
+//   });
+//
+//   precipArr.forEach(prob => {
+//     if (prob >= 0.5) {
+//       isItRaining = 2;
+//     } else if (prob >= 0.1) {
+//       isItRaining = 1;
+//     } else {
+//       isItRaining = 0;
+//     };
+//   });
+// };
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
