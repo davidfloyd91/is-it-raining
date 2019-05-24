d34e91dfa6490328d6a790eb52e4dd20e3b90c2e
davidfloyd91 <davidfloyd91@gmail.com>
2019-05-24 11:09:28 -0400

    geocoding in place

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // $ curl -X GET -H 'Content-Type: application/json' http://localhost:8081/weather
+// $ curl -X GET -H 'Content-Type: application/json' http://localhost:8081/location/11225
+
 // $ curl -X GET -H 'Content-Type: application/json' http://is-it-raining-env.mqxjxhgsyd.us-east-1.elasticbeanstalk.com/weather
 
 const express = require('express');
@@ -8,6 +10,7 @@ const app = express();
 const port = 8081;
 
 const darkSkyKey = process.env.DARK_SKY_API_KEY;
+const googleKey = process.env.GOOGLE_API_KEY;
 
 const corsOptions = {
   origin: '*'
@@ -20,7 +23,6 @@ app.get('/', (req, res) => {
 app.get('/weather/:lat,:lng', cors(corsOptions), (req, res) => {
   const lat = req.params.lat;
   const lng = req.params.lng;
-
   let data = '';
 
   const darkSkyOptions = {
@@ -30,6 +32,40 @@ app.get('/weather/:lat,:lng', cors(corsOptions), (req, res) => {
   };
 
   const request = https.request(darkSkyOptions, (response) => {
+    console.log(`STATUS: ${response.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+    response.setEncoding('utf8');
+    response.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      console.log('No more data in response.');
+
+      let parsed = JSON.parse(data);
+      res.send(parsed);
+    });
+  });
+
+  request.on('error', (e) => {
+    console.error(`Problem with request: ${e.message}`);
+  });
+
+  request.end();
+});
+
+app.get('/location/:zip', cors(corsOptions), (req, res) => {
+  const zip = req.params.zip;
+  let data = '';
+
+  const googleOptions = {
+    hostname: 'maps.googleapis.com',
+    path: `/maps/api/geocode/json?key=${googleKey}&address=${zip}`,
+    headers: { 'Content-Type': 'application/json' }
+  };
+
+  const request = https.request(googleOptions, (response) => {
     console.log(`STATUS: ${response.statusCode}`);
     console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
     response.setEncoding('utf8');
